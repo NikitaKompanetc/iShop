f689b8d6deaa722c7a6269caee74b4b913a0b76a
NikitaKompanetc <kni2012@ukr.net>
2021-05-31 16:02:26 +0300

    Update index.ts

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -3,13 +3,7 @@ import * as functions from 'firebase-functions';
 import firebase from 'firebase/app';
 import 'firebase/firestore';
 
-// // Start writing Firebase Functions
-// // https://firebase.google.com/docs/functions/typescript
-//
-export const helloWorld = functions.https.onRequest((request, response) => {
-  functions.logger.info('Hello logs!', { structuredData: true });
-  response.send('Hello from Firebase!');
-});
+
 
 export const scheduledFunctionPlainEnglish = functions.pubsub
   .schedule('every 5 minutes')
