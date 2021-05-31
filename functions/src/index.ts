9d4c87ae1f65ccd8cdc3a22473a83dc6299ee9d7
Vladyslav-Eduard Svystun <svvladyslaved@gmail.com>
2021-05-31 15:32:40 +0300

    add cloud func

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -1,15 +1,22 @@
-import * as functions from "firebase-functions";
+import * as functions from 'firebase-functions';
+
+import firebase from 'firebase/app';
+import 'firebase/firestore';
 
 // // Start writing Firebase Functions
 // // https://firebase.google.com/docs/functions/typescript
 //
 export const helloWorld = functions.https.onRequest((request, response) => {
-  functions.logger.info("Hello logs!", {structuredData: true});
-  response.send("Hello from Firebase!");
+  functions.logger.info('Hello logs!', { structuredData: true });
+  response.send('Hello from Firebase!');
 });
 
-export const scheduledFunctionPlainEnglish =
-functions.pubsub.schedule('every 5 minutes').onRun((context) => {
-    console.log('This will be run every 5 minutes!');
-});
-
+export const scheduledFunctionPlainEnglish = functions.pubsub
+  .schedule('every 5 minutes')
+  .onRun(async () => {
+    return (await firebase.firestore().collection('items').get()).docs.forEach(
+      (doc) => {
+        firebase.firestore().collection('items').doc(doc.id).delete();
+      }
+    );
+  });
